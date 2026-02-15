194a994f10d51bf412edf5ee93651cf038a4ddbc
popura <gzyoung2330351551@gmail.com>
2026-02-15 21:51:56 +0800

    teamdeathmatch beta2

--- src/main/java/com/cdp/codpattern/CodPattern.java
+++ src/main/java/com/cdp/codpattern/CodPattern.java
@@ -25,11 +25,10 @@ public class CodPattern {
         com.cdp.codpattern.config.tdm.CodTdmConfig.load(event.getServer());
     }
 
-    @SuppressWarnings("deprecation") // 保留旧命令以向后兼容，新命令请使用 /fpsm tdm
+    @SuppressWarnings("deprecation")
     @net.minecraftforge.eventbus.api.SubscribeEvent
     public void onRegisterCommands(net.minecraftforge.event.RegisterCommandsEvent event) {
         com.cdp.codpattern.command.CommandRegistration.register(event.getDispatcher());
-        // 旧命令 (已弃用): /codtdm create <mapName>
         // 新命令: /fpsm tdm create <mapName> (通过 TdmFpsmCommandHandler 注册)
         com.cdp.codpattern.command.CodTdmCommands.register(event.getDispatcher());
     }
