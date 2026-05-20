f4d6dddd1968d127e81e81d6dc9582e2480412a0
popura <gzyoung2330351551@gmail.com>
2026-05-20 18:02:46 +0800

    match beta53-zm clear mvp-0.7.6b

--- src/main/java/com/cdp/codpattern/network/tdm/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * @deprecated Legacy packet package retained for source compatibility. New code should use
- * {@code com.cdp.codpattern.network.match}.
- */
-@Deprecated(forRemoval = false)
-package com.cdp.codpattern.network.tdm;
